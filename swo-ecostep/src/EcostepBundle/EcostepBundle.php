2d0254e70bc1025600f3ae4f9d657d7f558c8f7b
louri45 <l.richard45@gmail.com>
2016-11-05 11:26:41 +0100

    Commit initial + ajout du EcostepBundle

--- swo-ecostep/src/EcostepBundle/EcostepBundle.php
+++ swo-ecostep/src/EcostepBundle/EcostepBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace EcostepBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class EcostepBundle extends Bundle
 {
 }
